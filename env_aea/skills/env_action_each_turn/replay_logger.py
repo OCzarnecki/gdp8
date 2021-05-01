49e3736021d4a7fdad16e2da635e1c34e8b4144a
Chun <scat8289@ox.ac.uk>
2021-05-01 19:29:24 +0800

    debugging dialogues

--- env_aea/skills/env_action_each_turn/replay_logger.py
+++ env_aea/skills/env_action_each_turn/replay_logger.py
@@ -1,15 +1,18 @@
+import os
 from datetime import datetime
 import json
 
 from packages.gdp8.skills.env_action_each_turn.environment import SimulationState
 
+
 class ReplayLogger:
     """ Responsible for logging simulation state, for later
         visualisation. """
 
-    def __init__(self, filename = None):
+    def __init__(self, filename=None):
         self._filename = filename
         self._file = None
+        self._folder = "logs"
 
     def initialize(self, simulation_state):
         self._open_file()
@@ -26,11 +29,13 @@ class ReplayLogger:
             self.close()
 
     def _open_file(self):
-        if self._filename == None:
+        if self._filename is None:
             filename = datetime.now().strftime(
-                    "logs/simlog_%Y-%m-%d_%H.%M.%S.json")
+                "logs/simlog_%Y-%m-%d_%H.%M.%S.json")
         else:
             filename = self._filename
+        if not os.path.exists(self._folder):
+            os.mkdir(self._folder)
         self._file = open(filename, "w")
 
     def _dump_json(self, obj):
